567585489ab145e2a02e55ad7af19c768897bdcb
Abdurrahman <capteinn@gmail.com>
2017-11-19 22:00:32 +0700

    Second Initial Commit

--- static/js/site.js
+++ static/js/site.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 	jQuery(document).ready(function($) {
 
 		    //$('#nav-main').scrollspy()
@@ -9,21 +7,3 @@
      		$('#news, .container').localScroll();
 
 	});
-	
-	
-
-=======
-
-	jQuery(document).ready(function($) {
-
-		    //$('#nav-main').scrollspy()
-		    
-		    // Localscrolling 
-    		$('#nav-main, .brand').localScroll();
-     		$('#news, .container').localScroll();
-
-	});
-	
-	
-
->>>>>>> a1c16752365901070e6d9df541985fe8a91324a7
